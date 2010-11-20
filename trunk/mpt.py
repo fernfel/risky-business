9e55bec03cc6041ea5a015f18812f2e8327ee104
privatekevin <privatekevin@2da158a2-47c9-9b7d-9360-a82bb54ff48b>
2010-11-20 19:09:23 +0000

    - started math

--- trunk/mpt.py
+++ trunk/mpt.py
@@ -1,1 +1,28 @@
-# this is a test
+import numpy
+
+class StockModel():
+    
+    def __init__(self, filename):
+        f = open(filename)
+        for line in f:        
+            date,open,high,low,close,volume,adjClose = line.trim().split(',')
+            if date.lower() != "date":
+                
+            
+        
+class PortfolioModel():
+    
+    def __init__(self, filename):
+        f = open(filename)
+        for line in f:        
+            date,open,high,low,close,volume,adjClose = line.trim().split(',')
+            if date.lower() != "date":
+                return
+                
+
+def calculateReturns(stockPrices):
+    return
+
+
+
+
