a23b8c12eb295cc9d1d6dbce0dff4d23a489ef8a
jdshaf@gmail.com <jdshaf@gmail.com@2da158a2-47c9-9b7d-9360-a82bb54ff48b>
2010-12-07 20:23:26 +0000

    added in beta scraping

--- getData.py
+++ getData.py
@@ -1,20 +1,32 @@
 # Get some yahoo finance yays
 
 import urllib2
+from BeautifulSoup import BeautifulSoup
+	
 
-file = open("S+P.txt")
-tickerList = list()
-while 1:
-	line = file.readline()
-	if not line:
-		break
-	tickerList.append(line.strip())	
-file.close()
-
-for ticker in tickerList:
-	print ticker
-	url = "http://ichart.finance.yahoo.com/table.csv?s="+ticker+"&a=00&b=1&c=1990&d=11&e=6&f=2010&g=d&ignore=.csv"
-	page = urllib2.urlopen(url)
-	file = open("data/"+ticker+'.csv', 'w')
-	file.write(page.read())
+if __name__ == "__main__":
+	file = open("S+P.txt")
+	tickerList = list()
+	while 1:
+		line = file.readline()
+		if not line:
+			break
+		tickerList.append(line.strip())	
 	file.close()
+	
+	for ticker in tickerList:
+		print ticker
+		url = "http://ichart.finance.yahoo.com/table.csv?s="+ticker+"&a=00&b=1&c=1980&d=11&e=6&f=2010&g=d&ignore=.csv"
+		data = urllib2.urlopen(url)
+		url2 = "http://finance.yahoo.com/q/ks?s="+ticker+"+Key+Statistics"
+		page = urllib2.urlopen(url2)
+		soup = BeautifulSoup(page)
+		data = soup.findAll("td", "yfnc_tabledata1")
+		beta =  data[31].text
+		
+		
+		file = open("data/"+ticker+'.csv', 'w')
+		
+		file.write(beta)
+		file.write(data.read())
+		file.close()
